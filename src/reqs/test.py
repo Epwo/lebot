3105d2567a24cca1b9b7edb15cc2666b09fddb65
epwo <ewann.postic@isen-ouest.yncrea.fr>
2025-02-14 22:39:59 +0000

    added Sonarr + updated readme for TODO

--- src/reqs/test.py
+++ src/reqs/test.py
@@ -1,6 +1,7 @@
 from RadarrClient import Radarr
 from ProwlarrClient import Prowlarr
 from QbitorrentClient import Qbitorrent
+from SonarrClient import Sonarr
 from dotenv import load_dotenv
 import os
 import json
@@ -11,9 +12,12 @@ load_dotenv()
 BASE_URL = os.getenv("BASE_URL")
 RADARR_API_KEY = os.getenv("RADARR_API_KEY")
 PROWLARR_API_KEY = os.getenv("PROWLARR_API_KEY")
+SONARR_API_KEY = os.getenv("SONARR_API_KEY")
+
 RADARR_URL = f"https://radarr.{BASE_URL}"
 PROWLARR_URL = f"https://prowlarr.{BASE_URL}"
 QBITTORRENT_URL = f"https://qbittorrent.{BASE_URL}"
+SONARR_URL = f"https://sonarr.{BASE_URL}"
 # radarr = Radarr(BaseUrl=RADARR_URL, api_key=RADARR_API_KEY)
 prowlarr = Prowlarr(BaseUrl=PROWLARR_URL, api_key=PROWLARR_API_KEY)
 
@@ -23,7 +27,10 @@ qbit.authenticate()
 
 # magnet = "https://prowlarr.media.swagman.fr/4/download?apikey=0f8a246ac39d49ca8c68f5b5a494fe43&link=eUUyWGlZTUpzcFRrVnptN3NIekEyaHdPYWdiMU5Qa1QvVVgva051NHE2R3g1ZTNnVzNNRHhzYzgxRndCTU13a1YxNjVaSW5sRGhaOCtTS0ZxaDNUV3kwN1VzSFVOdktoeWpLWGhSRDZjT3laOEZtcG5zTlRqN3ZhTGRMd0Q0ODU&file=Un+p%E2%80%99tit+truc+en+plus+2024+FRENCH+WEBRIP+"
 # print(qbit.add_torrent(magnet))
-results = prowlarr.search("Harry Potter", type="movie")
-print(results)
+# results = prowlarr.search("Harry Potter", type="movie")
+# print(results)
+
+sonarr = Sonarr(BaseUrl=SONARR_URL, api_key=SONARR_API_KEY)
+results = sonarr.get_series()
 with open("search_results.json", "w+") as file:
     json.dump(results, file, indent=4)
